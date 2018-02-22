5b27fe312ee0d1bdceaa2a1e72d4cf57437dcd30
skurp <wconnell93@gmail.com>
2018-02-22 09:53:29 -0800

    cleaned documentation

--- manifestMaker.R
+++ manifestMaker.R
@@ -29,34 +29,36 @@ out <- as.yaml(list(samples=split(replace(index.df, "name", NULL), index.df$name
 cat(out)
 
 # header -----------------------------------------------------------------
+reference_head <- c('reference_genome: /u/project/dkohn/wconnell/kohn/projects/guideseq/run/reference/Homo_sapiens_assembly19.fasta\n')
 
-#### ask for inputs ######
-
-
-#########################
+# get the output file location from user
 output_loc <- readline('What is the output folder name of this experiment?     ')
-reference_head <- c('reference genome: /u/project/dkohn/wconnell/kohn/projects/guideseq/run/reference/Homo_sapiens_assembly19.fasta\n')
 output_head <- paste0('output_folder: ./output/', output_loc, '\n')
 
+# get the location of fastq files from user
 undemultiplexed_path <- c('/u/project/dkohn/wconnell/kohn/globus/')
 forward <- paste0('/u/project/dkohn/wconnell/kohn/globus/', readline('What is the folder and file name of the forward (R1) read fastq? (e.g. MiSeq4/K562-Guideseq-VEGF-EMX1_S1_L001_R1_001.fastq.gz)     '))
 rev <- paste0('/u/project/dkohn/wconnell/kohn/globus/', readline('What is the folder and file name of the reverse (R2) read fastq? (e.g. MiSeq4/K562-Guideseq-VEGF-EMX1_S1_L001_R2_001.fastq.gz)     '))
 index1 <- paste0('/u/project/dkohn/wconnell/kohn/globus/', readline('What is the folder and file name of the index1 (I1) read fastq? (e.g. MiSeq4/K562-Guideseq-VEGF-EMX1_S1_L001_I1_001.fastq.gz)     '))
 index2 <- paste0('/u/project/dkohn/wconnell/kohn/globus/', readline('What is the folder and file name of the index2 (I2) read fastq? (e.g. MiSeq4/K562-Guideseq-VEGF-EMX1_S1_L001_I2_001.fastq.gz)     '))
 
+# paste the header together
 header <- paste0(reference_head,
                  output_head,
                  '\nbwa: bwa\nbedtools: bedtools\n\ndemultiplexed_min_reads: 1000',
-                 '\nundemultiplexed:\n    forward: ', forward, '\n',
+                 '\n\nundemultiplexed:\n    forward: ', forward, '\n',
                  '    reverse: ', rev, '\n',
                  '    index1: ', index1, '\n',
                  '    index2: ', index2, '\n',
                  '\n'
                  )
+
+# concatenate the header
 cat(header)
-
+# paste the header and samples together
 final <- paste0(header, out)
-
+# concatenate the final yaml
 cat(final)
-
-write(final, "MiSeq4/test.yaml")
+# write it out to location
+filename <- readline('What would you like to call this file?     ')
+write(final, filename)
